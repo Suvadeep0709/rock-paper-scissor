36910b05ac0b7ee3827606ce52879684f2c015a3
Suvadeep Mukherjee <isuvadeep@protonmail.com>
2022-09-13 16:11:39 +0530

    Task 1 completed

--- rock.java
+++ rock.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class rock {
     public static void main(String[] args) {
-        
+        System.out.println("Let's play Rock Paper Scissors.");
+        System.out.println("When I say 'shoot', Choose: rock, paper, or scissors.");
+        System.out.println("");
+        System.out.println("Are you ready? Write 'yes' if you are");
+        Scanner scan =new Scanner(System.in);
+        String choice=scan.nextLine();
+        if (choice.equals("yes")){
+            
+        }
     }
     
 }
